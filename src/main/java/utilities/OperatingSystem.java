9df7ef9379709550dbedb5b4b20f877f0c0ba8f2
Arnel Lindo Bagabagon Jr <42166977+abagabagon@users.noreply.github.com>
2020-04-26 02:45:25 +0800

    chore: transferred getCurrentDate Method

--- src/main/java/utilities/OperatingSystem.java
+++ src/main/java/utilities/OperatingSystem.java
@@ -1,9 +1,5 @@
 package utilities;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.Platform;
@@ -41,22 +37,6 @@ public class OperatingSystem {
 		
 		log.debug("Detected Operating System: " + operatingSystem);
 		return operatingSystem;
-	}
-	
-	/**
-	 * Gets current System Date.
-	 * 
-	 * @param  dateFormat format of the date. Follows format of
-	 *                    SimpleDateFormat.
-	 * @return current System Date.
-	 */
-
-	public static final String getCurrentDate(String dateFormat) {
-		log.trace("Getting Current Date");
-		DateFormat sdf = new SimpleDateFormat(dateFormat);
-		Date date = new Date();
-		log.trace("Retrieved Date: " + sdf.format(date).toString());
-		return sdf.format(date);
 	}
 
 }
