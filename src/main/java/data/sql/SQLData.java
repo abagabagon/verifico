289baeccda977db8d19902bff106b1bc2d5c96f4
Arnel Lindo Bagabagon Jr <42166977+abagabagon@users.noreply.github.com>
2020-08-02 00:47:23 +0800

    feature: add update, insert and delete commands

--- src/main/java/data/sql/SQLData.java
+++ src/main/java/data/sql/SQLData.java
@@ -42,21 +42,107 @@ public class SQLData {
 		this.connection = this.sqlDriver.getSQLConnection();
 	}
 	
-	public ResultSet execute(String sqlQuery) throws SQLTimeoutException {
-		this.log.info("I execute SQL Statement.");
-		try {
-			this.statement = this.connection.createStatement();
-			this.resultSet = this.statement.executeQuery(sqlQuery);
-		} catch(SQLException e) {
-			this.log.fatal("Encountered SQLException while executing SQL Statement!");
-			this.log.fatal("SQL Exception: " + e.getMessage());
-			this.log.fatal("SQL State: " + e.getSQLState());
-			this.log.fatal("Error Code: " + e.getErrorCode());
-		} catch(Exception e) {
-			this.log.fatal("Encountered Exception while executing SQL Statement!");
-			e.printStackTrace();
+	public ResultSet select(String sqlQuery) throws SQLTimeoutException {
+		this.log.info("I execute SQL SELECT Statement.");
+		if (sqlQuery.toLowerCase().contains("select")) {
+			try {
+				this.statement = this.connection.createStatement();
+				this.resultSet = this.statement.executeQuery(sqlQuery);
+			} catch(SQLException e) {
+				this.log.fatal("Encountered SQLException while executing SQL SELECT Statement!");
+				this.log.fatal("SQL Exception: " + e.getMessage());
+				this.log.fatal("SQL State: " + e.getSQLState());
+				this.log.fatal("Error Code: " + e.getErrorCode());
+			} catch(Exception e) {
+				this.log.fatal("Encountered Exception while executing SQL SELECT Statement!");
+				e.printStackTrace();
+			}
+		} else {
+			this.log.error("Statement is not a SELECT Statement.");
 		}
+
 		return this.resultSet;
+	}
+	
+	public void update(String sqlQuery) throws SQLTimeoutException {
+		this.log.info("I execute SQL UPDATE Statement.");
+		int updateCount = 0;
+		if (sqlQuery.toLowerCase().contains("update")) {
+			try {
+				this.statement = this.connection.createStatement();
+				updateCount = this.statement.executeUpdate(sqlQuery);
+			} catch(SQLException e) {
+				this.log.fatal("Encountered SQLException while executing SQL UPDATE Statement!");
+				this.log.fatal("SQL Exception: " + e.getMessage());
+				this.log.fatal("SQL State: " + e.getSQLState());
+				this.log.fatal("Error Code: " + e.getErrorCode());
+			} catch(Exception e) {
+				this.log.fatal("Encountered Exception while executing SQL UPDATE Statement!");
+				e.printStackTrace();
+			}
+			
+			if (updateCount > 0) {
+				this.log.info(updateCount + " SQL Records updated.");
+			} else {
+				this.log.error("No SQL Records were updated.");
+			}
+		} else {
+			this.log.error("Statement is not an UPDATE Statement.");
+		}
+	}
+	
+	public void insert(String sqlQuery) throws SQLTimeoutException {
+		this.log.info("I execute SQL INSERT Statement.");
+		int insertCount = 0;
+		if (sqlQuery.toLowerCase().contains("insert")) {
+			try {
+				this.statement = this.connection.createStatement();
+				insertCount = this.statement.executeUpdate(sqlQuery);
+			} catch(SQLException e) {
+				this.log.fatal("Encountered SQLException while executing SQL INSERT Statement!");
+				this.log.fatal("SQL Exception: " + e.getMessage());
+				this.log.fatal("SQL State: " + e.getSQLState());
+				this.log.fatal("Error Code: " + e.getErrorCode());
+			} catch(Exception e) {
+				this.log.fatal("Encountered Exception while executing SQL INSERT Statement!");
+				e.printStackTrace();
+			}
+			
+			if (insertCount > 0) {
+				this.log.info(insertCount + " SQL Records inserted.");
+			} else {
+				this.log.error("No SQL Records were inserted.");
+			}
+		} else {
+			this.log.error("Statement is not an INSERT Statement.");
+		}
+	}
+	
+	public void delete(String sqlQuery) throws SQLTimeoutException {
+		this.log.info("I execute SQL DELETE Statement.");
+		int insertCount = 0;
+		if (sqlQuery.toLowerCase().contains("delete") || sqlQuery.toLowerCase().contains("where")) {
+			try {
+				this.statement = this.connection.createStatement();
+				insertCount = this.statement.executeUpdate(sqlQuery);
+			} catch(SQLException e) {
+				this.log.fatal("Encountered SQLException while executing SQL DELETE Statement!");
+				this.log.fatal("SQL Exception: " + e.getMessage());
+				this.log.fatal("SQL State: " + e.getSQLState());
+				this.log.fatal("Error Code: " + e.getErrorCode());
+			} catch(Exception e) {
+				this.log.fatal("Encountered Exception while executing SQL DELETE Statement!");
+				e.printStackTrace();
+			}
+			
+			if (insertCount > 0) {
+				this.log.info(insertCount + " SQL Records deleted.");
+			} else {
+				this.log.error("No SQL Records were deleted.");
+			}
+		} else {
+			this.log.error("Statement is not a DELETE Statement or no WHERE conditions were provided.");
+		}
 	}
 	
 	public void closeResultSet() {
