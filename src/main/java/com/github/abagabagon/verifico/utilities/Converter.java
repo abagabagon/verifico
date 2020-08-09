f9d534840e959c856d4293b8f16dbc09d8af1067
Arnel Lindo Bagabagon Jr <42166977+abagabagon@users.noreply.github.com>
2020-08-09 20:16:37 +0800

    chore: restructure packages

---
+++ src/main/java/com/github/abagabagon/verifico/utilities/Converter.java
@@ -0,0 +1,63 @@
+package com.github.abagabagon.verifico.utilities;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Converter {
+	
+	private static Logger log;
+	
+	static {
+		log = LogManager.getLogger(Converter.class);
+		log.debug("Initializing Converter Class.");
+		log.debug("Successfully initialized Converter Class.");
+	}
+	
+	/**
+	 * Converts a String to Integer by parsing input string.
+	 * 
+	 * @param  input Input string for conversion to date format.
+	 * @return converted Integer
+	 */
+	
+	public static final int convertStringToInteger(String input) {
+		int index = 0;
+		try {
+			log.trace("Converting String to Integer.");
+			index = Integer.parseInt(input);
+		} catch (NumberFormatException e) {
+			log.error("Encountered NumberFormatException while converting String to Integer!");
+		} catch (Exception e) {
+			log.error("Encountered Exception while converting String to Integer!");
+		}
+		return index;
+	}
+	
+	/**
+	 * Converts a String to Date by parsing input string.
+	 * 
+	 * @param  stringDate input string for conversion to date format.
+	 * @param  dateFormat format of the date. Follows format of
+	 *                    SimpleDateFormat.
+	 * @return converted Date
+	 */
+
+	public static final Date convertStringToDate(String stringDate, String dateFormat) {
+		Date date = null;
+		try {
+			log.trace("Parsing String Date.");
+			date = new SimpleDateFormat(dateFormat).parse(stringDate);
+			log.trace("Successfully parsed String Date!");
+		} catch (ParseException e) {
+			log.error("Encountered ParseException while converting Date from String to Date Format!");
+		} catch (Exception e) {
+			log.error("Encountered Exception while converting Date from String to Date Format!");
+		}
+		return date;
+	}
+	
+}
---
+++ src/main/java/com/github/abagabagon/verifico/utilities/OperatingSystem.java
@@ -0,0 +1,42 @@
+package com.github.abagabagon.verifico.utilities;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Platform;
+
+public class OperatingSystem {
+	
+	private static Logger log;
+	
+	static {
+		log = LogManager.getLogger(OperatingSystem.class);
+		log.debug("Initializing OperatingSystem Class.");
+		log.debug("Successfully initialized OperatingSystem Class.");
+	}
+	
+	/**
+	 * Gets Operating System of Host System.
+	 * 
+	 * @return Operating System Platform
+	 */
+	
+	public static final Platform getOS() {
+		log.debug("Detecting Operating System.");
+		String operatingSystemName = System.getProperty("os.name").toLowerCase();
+		Platform operatingSystem = null;
+		
+		if (operatingSystemName.contains("win")) {
+			operatingSystem = Platform.WINDOWS;
+		} else if (operatingSystemName.contains("mac")) {
+			operatingSystem = Platform.MAC;
+		} else if(operatingSystemName.contains("nux")) {
+			operatingSystem = Platform.LINUX;
+		} else {
+			log.fatal("Unsupported Operating System. Please report issue to QA Team.");
+		}
+		
+		log.debug("Detected Operating System: " + operatingSystem);
+		return operatingSystem;
+	}
+
+}
