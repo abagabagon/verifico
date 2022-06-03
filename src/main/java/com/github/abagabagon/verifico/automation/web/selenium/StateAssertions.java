c65eea19fa231fd6997bc53faf17b9d48e7c258d
Arnel Lindo Bagabagon Jr <42166977+abagabagon@users.noreply.github.com>
2022-06-04 01:06:02 +0800

    feat(#158): implemented state assertion commands

--- src/main/java/com/github/abagabagon/verifico/automation/web/selenium/StateAssertions.java
+++ src/main/java/com/github/abagabagon/verifico/automation/web/selenium/StateAssertions.java
@@ -1,7 +1,10 @@
 package com.github.abagabagon.verifico.automation.web.selenium;
 
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -9,9 +12,10 @@ public class StateAssertions extends Commands {
 
 	protected WebDriver driver;
 	protected Logger log;
+	private WebElementFactory elementFactory;
 	private WaitCommands wait;
 	
-	private enum StateAssertionAction {
+	private enum StateAssertion {
 		DISPLAYED, NOT_DISPLAYED, ENABLED, DISABLED, SELECTED, DESELECTED
 	}
 	
@@ -20,9 +24,10 @@ public class StateAssertions extends Commands {
 		this.log = LogManager.getLogger(this.getClass());
 		this.driver = driver;
 		this.wait = wait;
+		this.elementFactory = new WebElementFactory(this.driver, this.wait);
 	}
 	
-	private boolean execute(StateAssertionAction stateAssertionAction, WebElement element, int size) {
+	private boolean execute(StateAssertion stateAssertionAction, WebElement element, int size) {
 		boolean status = false;
 		switch(stateAssertionAction) {
 		case DISPLAYED:
@@ -52,22 +57,304 @@ public class StateAssertions extends Commands {
 		return status;
 	}
 	
-	private boolean doCommand(StateAssertionAction stateAssertionAction, WebElement element, int size) {
+	private boolean doCommand(StateAssertion stateAssertionAction, WebElement element, int size) {
 		boolean status = this.execute(stateAssertionAction, element, size);
 		if(status) {
-			if (stateAssertionAction == StateAssertionAction.NOT_DISPLAYED) {
+			if (stateAssertionAction == StateAssertion.NOT_DISPLAYED) {
 				this.log.debug("I saw state of the Web Element as " + String.valueOf(stateAssertionAction) + ".");
 			} else {
 				this.log.debug("I saw state of the Web Element: \"" + element.toString() + "\" as " + String.valueOf(stateAssertionAction) + ".");
 			}
 		} else {
-			if (stateAssertionAction == StateAssertionAction.NOT_DISPLAYED) {
+			if (stateAssertionAction == StateAssertion.NOT_DISPLAYED) {
 				this.log.error("I didn't see state of the Web Element as " + String.valueOf(stateAssertionAction) + ".");
 			} else {
 				this.log.error("I didn't see state of the Web Element: \"" + element.toString() + "\" as " + String.valueOf(stateAssertionAction) + ".");
 			}
 			
 		}
+		return status;
+	}
+	
+	/**
+	 * Verifies if Web Element of specified Locator is displayed on Web Page.
+	 * 
+	 * @param	locator Locator of Web Element to check.
+	 * @return	<code>true</code> if Web Element is displayed on Web Page.
+	 * 			<code>false</code> if Web Element is not displayed on Web Page.
+	 */
+	
+	public final boolean see(By locator) {
+		WebElement element = this.elementFactory.createElement(locator);
+		boolean status = this.doCommand(StateAssertion.DISPLAYED, element, 0);
+		return status;
+	}
+	
+	/**
+	 * Verifies if Web Element of specified Locator within the context of the
+	 * Web Element of the specified Parent Locator is displayed on Web Page.
+	 * 
+	 * @param	parent	Locator of Parent Web Element.
+	 * @param	child	Locator of Child Web Element to check.
+	 * @return	<code>true</code> if Web Element is displayed on Web Page.
+	 * 			<code>false</code> if Web Element is not displayed on Web Page.
+	 */
+	
+	public final boolean see(By parent, By child) {
+		WebElement element = this.elementFactory.createElement(parent, child);
+		boolean status = this.doCommand(StateAssertion.DISPLAYED, element, 0);
+		return status;
+	}
+	
+	/**
+	 * Verifies if Web Element of specified Locator within the context of the
+	 * Parent Web Element is displayed on Web Page.
+	 * 
+	 * @param	parent	Parent Web Element.
+	 * @param	child	Locator of Child Web Element to check.
+	 * @return	<code>true</code> if Web Element is displayed on Web Page.
+	 * 			<code>false</code> if Web Element is not displayed on Web Page.
+	 */
+	
+	public final boolean see(WebElement parent, By child) {
+		WebElement element = this.elementFactory.createElement(parent, child);
+		boolean status = this.doCommand(StateAssertion.DISPLAYED, element, 0);
+		return status;
+	}
+	
+	/**
+	 * Verifies if Web Element of specified Locator is not displayed on Web Page.
+	 * 
+	 * @param	locator Locator of Web Element to check.
+	 * @return	<code>true</code> if Web Element is not displayed on Web Page.
+	 * 			<code>false</code> if Web Element is displayed on Web Page.
+	 */
+	
+	public final boolean dontSee(By locator) {
+		this.wait.waitForListElementToBeInvisible(locator);
+		List<WebElement> elements = this.driver.findElements(locator);
+		boolean status = this.doCommand(StateAssertion.NOT_DISPLAYED, null, elements.size());
+		return status;
+	}
+	
+	/**
+	 * Verifies if Web Element of specified Locator within the context of the
+	 * Web Element of the specified Parent Locator is not displayed on Web Page.
+	 * 
+	 * @param	parent	Locator of Parent Web Element.
+	 * @param	child	Locator of Child Web Element to check.
+	 * @return	<code>true</code> if Web Element is not displayed on Web Page.
+	 * 			<code>false</code> if Web Element is displayed on Web Page.
+	 */
+	
+	public final boolean dontSee(By parent, By child) {
+		WebElement parentElement = this.elementFactory.createElement(parent);
+		List<WebElement> childElements = parentElement.findElements(child);
+		boolean status = this.doCommand(StateAssertion.NOT_DISPLAYED, null, childElements.size());
+		return status;
+	}
+	
+	/**
+	 * Verifies if Web Element of specified Locator within the context of the
+	 * Parent Web Element is not displayed on Web Page.
+	 * 
+	 * @param	parent	Parent Web Element.
+	 * @param	child	Locator of Child Web Element to check.
+	 * @return	<code>true</code> if Web Element is not displayed on Web Page.
+	 * 			<code>false</code> if Web Element is displayed on Web Page.
+	 */
+	
+	public final boolean dontSee(WebElement parent, By child) {
+		List<WebElement> childElements = parent.findElements(child);
+		boolean status = this.doCommand(StateAssertion.NOT_DISPLAYED, null, childElements.size());
+		return status;
+	}
+	
+	/**
+	 * Verifies if Web Element of specified Locator is enabled on Web Page.
+	 * 
+	 * @param	locator Locator of Web Element to check.
+	 * @return	<code>true</code> if Web Element is enabled on Web Page.
+	 * 			<code>false</code> if Web Element is disabled on Web Page.
+	 */
+	
+	public final boolean seeEnabled(By locator) {
+		WebElement element = this.elementFactory.createElement(locator);
+		boolean status = this.doCommand(StateAssertion.ENABLED, element, 0);
+		return status;
+	}
+	
+	/**
+	 * Verifies if Web Element of specified Locator within the context of the
+	 * Web Element of the specified Parent Locator is enabled on Web Page.
+	 * 
+	 * @param	parent	Locator of Parent Web Element.
+	 * @param	child	Locator of Child Web Element to check.
+	 * @return	<code>true</code> if Web Element is enabled on Web Page.
+	 * 			<code>false</code> if Web Element is disabled on Web Page.
+	 */
+	
+	public final boolean seeEnabled(By parent, By child) {
+		WebElement element = this.elementFactory.createElement(parent, child);
+		boolean status = this.doCommand(StateAssertion.ENABLED, element, 0);
+		return status;
+	}
+	
+	/**
+	 * Verifies if Web Element of specified Locator within the context of the
+	 * Parent Web Element is enabled on Web Page.
+	 * 
+	 * @param	parent	Parent Web Element.
+	 * @param	child	Locator of Child Web Element to check.
+	 * @return	<code>true</code> if Web Element is enabled on Web Page.
+	 * 			<code>false</code> if Web Element is disabled on Web Page.
+	 */
+	
+	public final boolean seeEnabled(WebElement parent, By child) {
+		WebElement element = this.elementFactory.createElement(parent, child);
+		boolean status = this.doCommand(StateAssertion.ENABLED, element, 0);
+		return status;
+	}
+	
+	/**
+	 * Verifies if Web Element of specified Locator is disabled on Web Page.
+	 * 
+	 * @param	locator Locator of Web Element to check.
+	 * @return	<code>true</code> if Web Element is disabled on Web Page.
+	 * 			<code>false</code> if Web Element is enabled on Web Page.
+	 */
+	
+	public final boolean seeDisabled(By locator) {
+		this.wait.waitForListElementToBeInvisible(locator);
+		List<WebElement> elements = this.driver.findElements(locator);
+		boolean status = this.doCommand(StateAssertion.DISABLED, null, elements.size());
+		return status;
+	}
+	
+	/**
+	 * Verifies if Web Element of specified Locator within the context of the
+	 * Web Element of the specified Parent Locator is disabled on Web Page.
+	 * 
+	 * @param	parent	Locator of Parent Web Element.
+	 * @param	child	Locator of Child Web Element to check.
+	 * @return	<code>true</code> if Web Element is disabled on Web Page.
+	 * 			<code>false</code> if Web Element is enabled on Web Page.
+	 */
+	
+	public final boolean seeDisabled(By parent, By child) {
+		WebElement parentElement = this.elementFactory.createElement(parent);
+		List<WebElement> childElements = parentElement.findElements(child);
+		boolean status = this.doCommand(StateAssertion.DISABLED, null, childElements.size());
+		return status;
+	}
+	
+	/**
+	 * Verifies if Web Element of specified Locator within the context of the
+	 * Parent Web Element is disabled on Web Page.
+	 * 
+	 * @param	parent	Parent Web Element.
+	 * @param	child	Locator of Child Web Element to check.
+	 * @return	<code>true</code> if Web Element is disabled on Web Page.
+	 * 			<code>false</code> if Web Element is enabled on Web Page.
+	 */
+	
+	public final boolean seeDisabled(WebElement parent, By child) {
+		List<WebElement> childElements = parent.findElements(child);
+		boolean status = this.doCommand(StateAssertion.DISABLED, null, childElements.size());
+		return status;
+	}
+	
+	/**
+	 * Verifies if Web Element of specified Locator is selected on Web Page.
+	 * 
+	 * @param	locator Locator of Web Element to check.
+	 * @return	<code>true</code> if Web Element is selected on Web Page.
+	 * 			<code>false</code> if Web Element is deselected on Web Page.
+	 */
+	
+	public final boolean seeSelected(By locator) {
+		WebElement element = this.elementFactory.createElement(locator);
+		boolean status = this.doCommand(StateAssertion.SELECTED, element, 0);
+		return status;
+	}
+	
+	/**
+	 * Verifies if Web Element of specified Locator within the context of the
+	 * Web Element of the specified Parent Locator is selected on Web Page.
+	 * 
+	 * @param	parent	Locator of Parent Web Element.
+	 * @param	child	Locator of Child Web Element to check.
+	 * @return	<code>true</code> if Web Element is selected on Web Page.
+	 * 			<code>false</code> if Web Element is deselected on Web Page.
+	 */
+	
+	public final boolean seeSelected(By parent, By child) {
+		WebElement element = this.elementFactory.createElement(parent, child);
+		boolean status = this.doCommand(StateAssertion.SELECTED, element, 0);
+		return status;
+	}
+	
+	/**
+	 * Verifies if Web Element of specified Locator within the context of the
+	 * Parent Web Element is selected on Web Page.
+	 * 
+	 * @param	parent	Parent Web Element.
+	 * @param	child	Locator of Child Web Element to check.
+	 * @return	<code>true</code> if Web Element is selected on Web Page.
+	 * 			<code>false</code> if Web Element is deselected on Web Page.
+	 */
+	
+	public final boolean seeSelected(WebElement parent, By child) {
+		WebElement element = this.elementFactory.createElement(parent, child);
+		boolean status = this.doCommand(StateAssertion.SELECTED, element, 0);
+		return status;
+	}
+	
+	/**
+	 * Verifies if Web Element of specified Locator is deselected on Web Page.
+	 * 
+	 * @param	locator Locator of Web Element to check.
+	 * @return	<code>true</code> if Web Element is deselected on Web Page.
+	 * 			<code>false</code> if Web Element is selected on Web Page.
+	 */
+	
+	public final boolean seeDeselected(By locator) {
+		this.wait.waitForListElementToBeInvisible(locator);
+		List<WebElement> elements = this.driver.findElements(locator);
+		boolean status = this.doCommand(StateAssertion.DESELECTED, null, elements.size());
+		return status;
+	}
+	
+	/**
+	 * Verifies if Web Element of specified Locator within the context of the
+	 * Web Element of the specified Parent Locator is deselected on Web Page.
+	 * 
+	 * @param	parent	Locator of Parent Web Element.
+	 * @param	child	Locator of Child Web Element to check.
+	 * @return	<code>true</code> if Web Element is deselected on Web Page.
+	 * 			<code>false</code> if Web Element is selected on Web Page.
+	 */
+	
+	public final boolean seeDeselected(By parent, By child) {
+		WebElement parentElement = this.elementFactory.createElement(parent);
+		List<WebElement> childElements = parentElement.findElements(child);
+		boolean status = this.doCommand(StateAssertion.DESELECTED, null, childElements.size());
+		return status;
+	}
+	
+	/**
+	 * Verifies if Web Element of specified Locator within the context of the
+	 * Parent Web Element is deselected on Web Page.
+	 * 
+	 * @param	parent	Parent Web Element.
+	 * @param	child	Locator of Child Web Element to check.
+	 * @return	<code>true</code> if Web Element is deselected on Web Page.
+	 * 			<code>false</code> if Web Element is selected on Web Page.
+	 */
+	
+	public final boolean seeDeselected(WebElement parent, By child) {
+		List<WebElement> childElements = parent.findElements(child);
+		boolean status = this.doCommand(StateAssertion.DESELECTED, null, childElements.size());
 		return status;
 	}
 	
